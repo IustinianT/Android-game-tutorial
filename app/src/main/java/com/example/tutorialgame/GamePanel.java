87caa69d4c5d6cd6a4b58753984f66db5d481404
IustinianT <iusti2003@yahoo.com>
2024-07-04 19:14:15 +0100

    Initialise project

---
+++ app/src/main/java/com/example/tutorialgame/GamePanel.java
@@ -0,0 +1,90 @@
+package com.example.tutorialgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Paint redPaint = new Paint();
+    private SurfaceHolder holder;
+    private float x,y;
+    private ArrayList<RndSquare> squares = new ArrayList<>();
+    private Random rand = new Random();
+
+    public GamePanel(Context context) {
+        super(context);
+        holder = getHolder();
+        holder.addCallback(this);
+        redPaint.setColor(Color.RED);
+    }
+
+    private void render(){
+        Canvas c = holder.lockCanvas();
+        // set canvas color to black
+        c.drawColor(Color.BLACK);
+
+        for(RndSquare square : squares) {
+            square.draw(c);
+        }
+
+        // send canvas to be rendered
+        holder.unlockCanvasAndPost(c);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            PointF pos = new PointF(event.getX(), event.getY());
+            int color = Color.rgb(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+            int size = 25 + rand.nextInt(100);
+
+            squares.add(new RndSquare(pos, color, size));
+
+            render();
+        }
+        return true;
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        render();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    private class RndSquare {
+        private PointF pos;
+        private int size;
+        private Paint paint;
+        public RndSquare(PointF pos, int color, int size) {
+            this.pos = pos;
+            this.size = size;
+            paint = new Paint();
+            paint.setColor(color);
+        }
+
+        public void draw(Canvas c) {
+            c.drawRect(pos.x, pos.y, pos.x+size, pos.y+size, paint);
+        }
+    }
+}
